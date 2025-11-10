c963a35c96cc97dc51d4134e06468549c27675ba
Студенческая <@rbiu.local>
2025-11-10 10:51:06 +0500

    added breeze and some changes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,20 +8,30 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/reports', function () {
-//     return view('report.index');
-// })->name('reports.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    
+    Route::get('/reports/create', function () {
+        return view('report.create');
+    })->name('reports.create');
+    
+    Route::delete('/reports/{report}', [ReportController::class, 'destroy'])->name('report.destroy');
+    
+    Route::post('/reports', [ReportController::class, 'store'])->name('report.store');
+    
+    Route::get('/reports/{report}/edit', [ReportController::class, 'edit'])->name('reports.edit');
+    Route::put('/reports/{report}', [ReportController::class, 'update'])->name('reports.update');
+});
 Route::get('/reports', [ReportController::class, 'index'])->name('report.index');
 
-Route::get('/reports/create', function () {
-    return view('report.create');
-})->name('reports.create');
 
 
-Route::delete('/reports/{report}', [ReportController::class, 'destroy'])->name('report.destroy');
 
-Route::post('/reports', [ReportController::class, 'store'])->name('report.store');
-
-Route::get('/reports/{report}/edit', [ReportController::class, 'edit'])->name('reports.edit');
-Route::put('/reports/{report}', [ReportController::class, 'update'])->name('reports.update');
+require __DIR__.'/auth.php';
